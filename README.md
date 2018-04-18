cb9a3dbf4719d7b01767f0f1eff1834c9d6f49e9
tiago.paranhos <tiagoprn@gmail.com>
2018-04-17 22:15:57 -0300

    Bootstraping the models and the Django Admin.

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 Personal notes accessible from Django Admin and a Rest API, with versioning and markdown support. 
 
 Created based on this recipe: https://github.com/tiagoprn/experiments/tree/master/django/drf_book
+
+## How to run the project 
+
+```
+$ pip install -r requirements.txt (please do this on a virtualenv)
+$ cd notes_api
+$ python manage.py migrate
+$ python manage.py createsuperuser (this user will be used to login to the API, which will support permissions on a few iterations on the future).
+$ python manage.py collectstatic (this will copy the django-admin/drf console static files to /static)
+$ cd static && python -m http.server 8088 & (this will run python's http server to serve the static files)
+$ python manage.py runserver
+```
+
---
+++ notes_api/notes/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='tags', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = [['owner', 'name'],
+                          ['owner', 'created']]
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='notes', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    uri = models.URLField()
+    name = models.CharField(max_length=200, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    tags = models.ManyToManyField(Tag,
+                                  related_name='notes',
+                                  through='NoteTag',
+                                  through_fields=('note', 'tag'))
+
+    class Meta:
+        ordering = ('-created',)
+        unique_together = (('owner', 'uri'),)
+        index_together = [['owner', 'uri'],
+                          ['owner', 'name'],
+                          ['owner', 'created']]
+
+    def __str__(self):
+        return self.name or self.url
+
+
+class NoteTag(models.Model):
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Link: {}, Tag: {}'.format(self.note.name, self.tag.name)
