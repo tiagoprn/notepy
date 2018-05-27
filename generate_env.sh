8f3d6d428626f73afe6d653c8626dfe73d7e37e9
tiago.paranhos <tiagoprn@gmail.com>
2018-05-27 20:02:04 -0300

    fixed hostname.

--- generate_env.sh
+++ generate_env.sh
@@ -5,7 +5,7 @@ NOTEPY_SECRET_KEY=$(./random_password --num_chars 50)
 POSTGRES_USER=notepy
 POSTGRES_DB=notepy
 PGPASSWORD=$(./random_password --num_chars 10)  # This variable name for the pg password is for convenience on psql
-POSTGRES_HOST=localhost
+POSTGRES_HOST=127.0.0.1
 POSTGRES_PORT=5432
 TZ=Etc/UTC
 EOF
